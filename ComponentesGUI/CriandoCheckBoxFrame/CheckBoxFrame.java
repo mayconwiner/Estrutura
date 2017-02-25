15f92906fb056c545b148234669cf8d2060b470e
Maycon Douglas <mayconwiner@gmail.com>
2017-02-25 00:57:18 -0300

    Casse Checkbox Pronta

---
+++ ComponentesGUI/CriandoCheckBoxFrame/CheckBoxFrame.java
@@ -0,0 +1,57 @@
+package CriandoCheckBoxFrame;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class CheckBoxFrame extends JFrame {
+	private JTextField textField; // exibie o texto na alteração de fontes
+	private JCheckBox negritoCheckBox;// para selecionar negrito
+	private JCheckBox italicoCheckBox; // Para Italico 
+
+	// Construtor CheckbosFrame adiciona Jcheckbox ao Jframe
+	public CheckBoxFrame() {
+		//super("JCheckBox teste");
+		setLayout(new FlowLayout()); // configura o layout do frame
+
+		// configura o JtextField e sua fonte
+		textField = new JTextField("Qual o estilo da fonte", 23);
+		textField.setFont(new Font("Serif", Font.PLAIN, 14));
+		add(textField);
+
+		negritoCheckBox = new JCheckBox("Negrito");
+		italicoCheckBox = new JCheckBox("Italico");
+		add(negritoCheckBox);
+		add(italicoCheckBox);
+
+		CheckBoxHandler handler = new CheckBoxHandler();
+		negritoCheckBox.addItemListener(handler);
+		italicoCheckBox.addItemListener(handler);
+
+	}
+
+	private class CheckBoxHandler implements ItemListener
+	{
+
+		public void itemStateChanged(ItemEvent event) {
+			Font font = null; //armazena a nova fonte
+			
+			//determina que checkboxes estão marcados e cria a font
+			
+			if (negritoCheckBox.isSelected()&& italicoCheckBox.isSelected())
+				font = new Font("Serif",Font.BOLD  + Font.ITALIC,15);
+			else if (negritoCheckBox.isSelected())
+				font = new Font("Serif",font.BOLD,14);
+			else if (italicoCheckBox.isSelected())
+				font = new Font("Serif",font.ITALIC,14);
+			else
+				font = new Font("Serif",Font.PLAIN,14);
+			textField.setFont(font); // configura a fonte do TextField
+		}//fim metodo
+	}//fim classe checkboxhandler
+}//fim da classe CheckBoxFrame
---
+++ ComponentesGUI/CriandoCheckBoxFrame/CheckBoxTeste.java
@@ -0,0 +1,15 @@
+package CriandoCheckBoxFrame;
+
+import javax.swing.JFrame;
+
+public class CheckBoxTeste {
+
+	public static void main(String[] args) {
+		CheckBoxFrame janelaCheckbox = new CheckBoxFrame();
+		janelaCheckbox.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janelaCheckbox.setTitle("Exemplos com CheckBox");
+		janelaCheckbox.setSize(300,250);
+		janelaCheckbox.setVisible(true);
+	}
+
+}
